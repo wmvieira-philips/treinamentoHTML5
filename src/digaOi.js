2c2dd872a63263c4b0da0339018ddfd90c2a98f5
William Martins Vieira <wmvieira@whebdc.com.br>
2016-08-15 16:12:53 -0300

    OSFinal

--- src/digaOi.js
+++ src/digaOi.js
@@ -1,3 +1,13 @@
 function digaOi(){
-alert("oi")
+alert("oi");
+}
+
+function calcularNotas(var a){
+var soma  = 1;//(nota1 + nota2 + nota3 + nota4) / 4;
+alert("Aprovado com médica " + soma);
+  if(soma >= 7){
+    alert("Aprovado com médica " + soma);
+  } else {
+    alert("Reprovado com médica " + soma);
+  }
 }
